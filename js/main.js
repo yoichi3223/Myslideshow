d2da4c3a72026401218b847fc7c0fefb0a62f122
Yoichi Hashimoto <yoichi3223@gmail.com>
2021-02-24 23:39:17 +0900

    firstcommit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+"use strict";
+{
+  const images = [
+    "img/pic00.png",
+    "img/pic01.png",
+    "img/pic02.png",
+    "img/pic03.png",
+    "img/pic04.png",
+    "img/pic05.png",
+    "img/pic06.png",
+    "img/pic07.png",
+    "img/pic08.png",
+  ];
+
+  let currentIndex = 0;
+
+  const mainImage = document.getElementById("main");
+  mainImage.src = images[currentIndex];
+
+  images.forEach((image) => {
+    const img = document.createElement("img");
+    img.src = image;
+    const li = document.createElement("li");
+    li.appendChild(img);
+    const ul = document.querySelector(".thumnails");
+    ul.appendChild(li);
+    // li.src = image;
+    // ul.appendChild(li);
+  });
+}
